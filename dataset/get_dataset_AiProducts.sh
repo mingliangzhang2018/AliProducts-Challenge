ffa6fe9301c984e0b4131edfddd48aa3a8a96f61
ZML <38106776+mingliangzhang2018@users.noreply.github.com>
2023-02-14 19:11:25 +0800

    Create get_dataset_AiProducts.sh

--- dataset/get_dataset_AiProducts.sh
+++ dataset/get_dataset_AiProducts.sh
@@ -1,28 +1,2 @@
 ﻿#!/usr/bin/env bash
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/train.json
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/val.json
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/product_tree.json
-wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/test.json
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/002521585550641/train_val.part0.tar.gz
-tar -zxvf train_val.part0.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/100001585554035/train_val.part1.tar.gz
-tar -zxvf train_val.part1.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/200001585540031/train_val.part2.tar.gz
-tar -zxvf train_val.part2.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/300001585559032/train_val.part3.tar.gz
-tar -zxvf train_val.part3.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/400001585578035/train_val.part4.tar.gz
-tar -zxvf train_val.part4.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/500001585599038/train_val.part5.tar.gz
-tar -zxvf train_val.part5.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/600001585536030/train_val.part6.tar.gz
-tar -zxvf train_val.part6.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/700001585524033/train_val.part7.tar.gz
-tar -zxvf train_val.part7.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/800001585502035/train_val.part8.tar.gz
-tar -zxvf train_val.part8.tar.gz
-wget https://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/900001585552031/train_val.part9.tar.gz
-tar -zxvf train_val.part9.tar.gz
-wget http://tianchi-competition.oss-cn-hangzhou.aliyuncs.com/231780/AliProducts/test.tar.gz
-tar -zxvf test.tar.gz
 
